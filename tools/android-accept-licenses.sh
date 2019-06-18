a6ee2a8b65b7db007c7ac647fdff0752d4b17c28
derwasp <nbcwasp@gmail.com>
2019-06-18 19:19:58 +0000

    android-java-6-24.4.1[skip appveyor]

--- tools/android-accept-licenses.sh
+++ tools/android-accept-licenses.sh
@@ -6,25 +6,9 @@ set licenses [lindex $argv 1]
 
 spawn {*}$cmd
 expect {
-    "Do you accept the license '*'*" {
+  "Do you accept the license '*'*" {
         exp_send "y\r"
         exp_continue
-    }
-    "Accept? (y/N): " {
-        exp_send "y\r"
-        exp_continue
-    }
-    "Review licenses that have not been accepted (y/N)? " {
-        exp_send "y\r"
-        exp_continue
-    }
-    eof
-}
-
-lassign [wait] pid spawnid os_error waitvalue
-
-if {$os_error == 0} {
-    exit $waitvalue
-} else {
-    exit 1
+  }
+  eof
 }
